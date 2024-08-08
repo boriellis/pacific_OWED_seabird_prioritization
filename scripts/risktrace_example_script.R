e6efcc846fdb11ef5a70fa4b70cfcc1a3ec91015
boriellis <aaellis@ucsc.edu>
2024-08-08 13:56:29 -0700

    adding annotations

--- scripts/risktrace_example_script.R
+++ scripts/risktrace_example_script.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
 risk_trace <- expand_grid(
-  e = c(0.1, 0.5, 0.9),
-  s = c(2/3, 1, 3/2),
-  t = 10^seq(-0.25, 0.25, length.out = 5)
+  e = c(0.1, 0.5, 0.9), #this is three values representing a low, mid, and high exposure
+  s = c(2/3, 1, 3/2), #these are a low, mid, and high sensitivity
+  t = 10^seq(-0.25, 0.25, length.out = 5) #this is setting up some numbers for threat 
 ) %>% 
   mutate(i = seq_along(e),
          es = e * s,
