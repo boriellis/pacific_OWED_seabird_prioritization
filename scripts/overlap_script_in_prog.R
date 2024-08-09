97c7080b701254b07c2d8fdf3faae099fb3b843d
boriellis <aaellis@ucsc.edu>
2024-08-09 15:34:25 -0700

    updating script to set up for the loop

--- scripts/overlap_script_in_prog.R
+++ scripts/overlap_script_in_prog.R
@@ -10,5 +10,52 @@ packages<- c("sf", "terra", "dplyr")
 
 pacman::p_load(packages, character.only = TRUE); rm(packages)
 
-# Part 2: Import data -------------------------------------------------------
-calls <- vect("data/BOEM_Wind_Planning_Areas_04_29_2024.shp") 
+# Part 2: Import data and make parcels  ---------------------------------------
+
+##WIND ENERGY AREAS
+calls <- vect("data/BOEM_Wind_Planning_Area_Outlines_04_29_2024.shp") #this contains the two oregon areas
+leases <- vect("data/BOEM_Wind_Lease_Outlines_06_06_2024.shp") #this contains the five CA leases
+
+#change projection
+crs <- "+proj=omerc +lat_0=39 +lonc=-125 +alpha=75 +gamma=75 +k=0.9996 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs" #this is the coordinate system for the density data
+calls <- project(calls, crs)
+leases <- project(leases, crs)
+
+#trim to individual parcels
+#OR
+OCSP0566 <- calls[calls$ADDITIONAL == "Oregon PSN - OCS-P 0566"] 
+OCSP0567 <- calls[calls$ADDITIONAL == "Oregon PSN - OCS-P 0567"]
+#CA
+OCSP0561 <- leases[leases$LEASE_NUMB == "OCS-P 0561"] #humboldt 
+OCSP0562 <- leases[leases$LEASE_NUMB == "OCS-P 0562"] #humboldt 
+OCSP0563 <- leases[leases$LEASE_NUMB == "OCS-P 0563"] #morro 
+OCSP0564 <- leases[leases$LEASE_NUMB == "OCS-P 0564"] #morro 
+OCSP0565 <- leases[leases$LEASE_NUMB == "OCS-P 0565"] #morro 
+
+
+# Part 3: Set up for the loop ---------------------------------------------
+
+#make a vector of just the species codes/groups
+birdcodes <- list.files("data/densities") #make a list of all the file names
+birdcodes <- regmatches(birdcodes, regexpr("[^_]+", birdcodes)) #Getting just the species codes - that operator is saying to extract everything up to the first underscore 
+cleancodes <- birdcodes[!duplicated(birdcodes)] #make a list without duplicates
+
+#making a dataframe to house the proportions
+propsdf <- data.frame(matrix(ncol = 8, nrow = length(cleancodes))) #make the empty dataframe
+rownames(propsdf) <- cleancodes #setting the rownames to the species codes
+colnames(propsdf) <- c("totdens_OCSP0561", "totdens_OCSP0562", "totdens_OCSP0563", "totdens_OCSP0564", "totdens_OCSP0565", "totdens_OCSP0566", "totdens_OCSP0567", "totdens_region") #making colnames to house the densities for each area
+
+
+
+# Part 4: Loop to calculate density of birds in each area -----------------
+
+
+                        
+                        
+                      
+
+
+
+
+
+
