8b38d6f14bae8acee7c88931244cad45f38c1c2e
boriellis <aaellis@ucsc.edu>
2024-08-17 13:04:31 -0700

    renaming and adding scripts

---
+++ scripts/02_data_cleaning.R
@@ -0,0 +1,5 @@
+###############################################################################
+# Seabird species prioritization framework for offsetting offshore wind energy impacts in the California Current   ######################
+# Code Author: Aspen Ellis (aaellis@ucsc.edu) ##
+# Manuscript Authors:  #
+###############################################################################
