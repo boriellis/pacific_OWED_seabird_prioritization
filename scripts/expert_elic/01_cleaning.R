4eecc694822fb59476c04ab276203b1ebb202916
boriellis <aaellis@ucsc.edu>
2025-04-11 15:28:31 -0700

    upload a preliminary dataset and the start of a script for expert elicitation cleaning

---
+++ scripts/expert_elic/01_cleaning.R
@@ -0,0 +1,16 @@
+###############################################################################
+# Seabird species prioritization framework for offsetting offshore wind energy impacts in the California Current  
+#expert elicitation data cleaning scratch script
+######################
+# Code Author: Aspen Ellis (aaellis@ucsc.edu) ##
+# Manuscript Authors:  #
+###############################################################################
+
+# Part 1: Load Packages -------------------------------------------------------
+
+# Load packages
+packages<- c("tidyverse", "here")
+
+pacman::p_load(packages, character.only = TRUE); rm(packages)
+
+# Part 2: Import and Clean Data -----------------------------------------------
