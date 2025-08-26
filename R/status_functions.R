a3460c9a08860bb73358dff3cf5dfa65aa174a07
boriellis <aaellis@ucsc.edu>
2025-08-26 12:55:43 -0700

    work on status fx

--- R/status_functions.R
+++ R/status_functions.R
@@ -1,1 +1,6 @@
-#this script is where the functions that deal with the status values will go 
+#these are the functions in the status branch of the work flow
+
+
+# CLEAN STATUS SCORES -----------------------------------------------------
+
+#this function combines pull the appropriate IUCN statuses from the raw file and match them to the right names, make a new processed data frame of common name, species code, scientific name local (the one we use), scientific name reference (the one we're pulling the IUCN score from in the raw sheet), & IUCN status
