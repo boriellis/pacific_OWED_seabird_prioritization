048a39975c3abd2b35460483e8294a7b589009d3
boriellis <aaellis@ucsc.edu>
2025-10-09 14:08:42 -0700

    update final results table

--- scripts/04_multiply.R
+++ scripts/04_multiply.R
@@ -5,6 +5,16 @@ exposure <- read_rds(here::here("output/exposure_20sims.rds"))
 sensitivity <- read_rds(here::here("output/sensitivity_sum.rds"))
 status <- read_rds(here::here("output/status.rds"))
 
+# Table
+priority_table <- calc_priority(exposure, 
+                                sensitivity, 
+                                status, 
+                                w = c(3, 2, 1)) %>% 
+  left_join(exposure, by = c("alpha_code", "region")) %>% 
+  left_join(sensitivity, by = "alpha_code") %>% 
+  left_join(status, by = "alpha_code")
+  
+
 # Max's attempt to plot
 foo <- calc_priority(exposure, sensitivity, status, w = c(3, 2, 1))
 foo_long <- foo %>% 
