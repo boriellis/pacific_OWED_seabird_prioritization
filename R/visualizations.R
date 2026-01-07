97fac83f24a642babc1b340100c10f1c407b36fe
boriellis <aaellis@ucsc.edu>
2026-01-07 15:53:46 -0800

    refactor clean priority values (fixes #50)

--- R/visualizations.R
+++ R/visualizations.R
@@ -2,6 +2,64 @@
 
 
 
+
+
+
+#' Clean priority values for results table
+#'
+#' @param raw_scores "output/priority_scores_1000.rds"
+#' @param se "output/sensitivity_sum.rds"
+#' @param st "output/status.rds"
+#' @param selection is the region you want (e.g., "CA")
+#'
+#' @returns a formatted table to save as a csv that can be easily added to the manuscript
+#' @export
+#'
+#' @examples
+clean_priority_vals <- function(raw_scores, se, st, selection){
+  #set up results df for selected region
+  regional_scores <- raw_scores %>% 
+    filter(region == selection) %>% 
+    mutate(mean_raw_overlap = map_dbl(outliers_rm, mean),
+           lwr_raw_overlap = map_dbl(outliers_rm, min), #this is technically the 2.5% quantile since I already capped values below that to that value at the winzorization step
+           upr_raw_overlap = map_dbl(outliers_rm, max) #97.5% quantile of the raw. 
+    ) %>%
+    select(alpha_code, common_name, mean_raw_overlap, lwr_raw_overlap, upr_raw_overlap, CV, DV, rl_category, ess, ess_lwr, ess_upr) %>% 
+    arrange(desc(ess)) %>%         
+    mutate(pri_rank = row_number())
+  #make a df of data deficient species
+  dd_species <- se %>% 
+    left_join(st, by = "alpha_code") %>% 
+    anti_join(regional_scores, by = "alpha_code") %>% 
+    mutate(mean_raw_overlap = NA_real_,
+           lwr_raw_overlap = NA_real_,
+           upr_raw_overlap = NA_real_,
+           ess = NA_real_,
+           ess_lwr = NA_real_,
+           ess_upr = NA_real_
+    ) %>% 
+    select(alpha_code, common_name, mean_raw_overlap, lwr_raw_overlap, upr_raw_overlap, CV, DV, rl_category, ess, ess_lwr, ess_upr)
+ 
+  #add the two together
+  total_region_table <- bind_rows(regional_scores, dd_species)
+  
+  #format for output to manuscript table
+  formatted_results_table <- total_region_table %>% 
+    mutate(mean_exp_percent = mean_raw_overlap*100,
+           lwr_exp_percent = lwr_raw_overlap*100,
+           upr_exp_percent = upr_raw_overlap *100,
+           CV = round(CV, 3),
+           DV = round(DV, 3),
+           exp_ci = sprintf("%.3f (%.3f, %.3f)", mean_exp_percent, lwr_exp_percent, upr_exp_percent),
+           ess_ci = sprintf("%.3f (%.3f, %.3f)", ess, ess_lwr, ess_upr)
+    ) %>% 
+    select(common_name, exp_ci, CV, DV, rl_category, ess_ci, pri_rank)
+}
+
+  
+
+
+
 #' Returns 1000 ranks, not priority values
 #'
 #' @param e "output/cleaned_exposure_1000sims.rds" 
@@ -35,7 +93,3 @@ priority_mc <- function(e,
     list_rbind()
 }
 
-
-
-
-# saveRDS(result, "paper/112priority_ranks_1000_for_plots.rds")
--- scripts/05_plot.R
+++ scripts/05_plot.R
@@ -6,59 +6,25 @@ source(here::here("R/visualizations.R"))
 
 
 
+###########################################################################
+#####################                           ###########################
+#####################       RESULTS TABLES      ###########################
+#####################                           ###########################
+###########################################################################
 
-#results table for CA
 
+#load data
 raw_scores <- read_rds(here::here("output/priority_scores_1000.rds"))
-
-CA_table <- raw_scores %>% 
-  filter(region == "CA") %>% 
-  mutate(mean_raw_overlap = map_dbl(outliers_rm, mean),
-         lwr_raw_overlap = map_dbl(outliers_rm, min), #this is technically the 2.5% quantile since I already capped values below that to that value at the winzorization step
-         upr_raw_overlap = map_dbl(outliers_rm, max) #97.5% quantile of the raw. 
-         ) %>%
-  select(alpha_code, common_name, mean_raw_overlap, lwr_raw_overlap, upr_raw_overlap, CV, DV, rl_category, ess, ess_lwr, ess_upr) %>% 
-  arrange(desc(ess)) %>%         
-  mutate(pri_rank = row_number())
-
-
-#add in data deficient species
-
 se <- read_rds(here::here("output/sensitivity_sum.rds"))
 st <- read_rds(here::here("output/status.rds"))
 
-dd_species <- se %>% 
-  left_join(st, by = "alpha_code") %>% 
-  anti_join(CA_table, by = "alpha_code") %>% 
-  mutate(mean_raw_overlap = NA_real_,
-         lwr_raw_overlap = NA_real_,
-         upr_raw_overlap = NA_real_,
-         ess = NA_real_,
-         ess_lwr = NA_real_,
-         ess_upr = NA_real_
-         ) %>% 
-  select(alpha_code, common_name, mean_raw_overlap, lwr_raw_overlap, upr_raw_overlap, CV, DV, rl_category, ess, ess_lwr, ess_upr)
-  
+#CA
+formatted_results_table_CA <- clean_priority_vals(raw_scores, se, st, "CA")
+write_csv(formatted_results_table_CA, "paper/CA_results_table.csv")
 
-
-total_CA_table <- bind_rows(CA_table, dd_species)
-
-formatted_results_table <- total_CA_table %>% 
-  mutate(mean_exp_percent = mean_raw_overlap*100,
-         lwr_exp_percent = lwr_raw_overlap*100,
-         upr_exp_percent = upr_raw_overlap *100,
-         CV = round(CV, 3),
-         DV = round(DV, 3),
-         exp_ci = sprintf("%.3f (%.3f, %.3f)", mean_exp_percent, lwr_exp_percent, upr_exp_percent),
-         ess_ci = sprintf("%.3f (%.3f, %.3f)", ess, ess_lwr, ess_upr)
-         ) %>% 
-  select(common_name, exp_ci, CV, DV, rl_category, ess_ci, pri_rank)
-  
-
-
-#save to paper folder
-write_csv(formatted_results_table, "paper/CA_results_table.csv")
-
+#all lease areas
+formatted_results_table_all <- clean_priority_vals(raw_scores, se, st, "all")
+write_csv(formatted_results_table_all, "paper/POCS_results_table.csv")
 
 
 
